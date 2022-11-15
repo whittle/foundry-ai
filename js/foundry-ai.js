950bbdae3e2691845c9b6e4c84c5e80a5de26fc7
Jason Whittle <jason.whittle@gmail.com>
2022-11-14 22:46:38 -0500

    Register a Foundry setting for the server URL.

--- js/foundry-ai.js
+++ js/foundry-ai.js
@@ -1,19 +1,43 @@
+const MODULE_NAME = 'foundry-ai';
+const SERVER_URL_SETTING = 'server_url';
+
 Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
-    registerPackageDebugFlag(FoundryAI.ID);
+    registerPackageDebugFlag(MODULE_NAME);
 });
 
-Hooks.once('init', async function() {
-    FoundryAI.log(false, "Hello from the local dev!");
-});
+Hooks.once('setup', async () => {
+    FoundryAI.debug('Initializing');
+    window.FoundryAI = FoundryAI;
+    window.FoundryAI.registerSettings();
+})
 
 class FoundryAI {
-    static ID = 'foundry-ai';
+
+    static debug(...args) {
+	this.log(false, ...args);
+    }
+
+    static error(...args) {
+	this.log(true, ...args);
+    }
 
     static log(force, ...args) {
-	const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(this.ID);
+	const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(MODULE_NAME);
 
 	if (shouldLog) {
-	    console.log(this.ID, '|', ...args);
+	    console.log(MODULE_NAME, '|', ...args);
 	}
+    }
+
+    static registerSettings() {
+	game.settings.register(MODULE_NAME, SERVER_URL_SETTING, {
+	    name: game.i18n.localize('foundry-ai.settings.server_url.name'),
+	    hint: game.i18n.localize('foundry-ai.settings.server_url.hint'),
+	    scope: 'client',
+	    config: true,
+	    requiresReload: true,
+	    type: String,
+	    default: ''
+	});
     }
 }
